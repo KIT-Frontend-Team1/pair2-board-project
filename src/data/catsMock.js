492ebd457f7d1a036c0642da82f51e8435de528e
Jaewon <grayhound0801@gmail.com>
2023-05-27 19:52:21 +0900

    feat: Pagination 기능 구현

--- src/data/catsMock.js
+++ src/data/catsMock.js
@@ -1,32 +1,32 @@
 const { faker } = require("@faker-js/faker");
 
 const randomId = {
-  generate() {
-    return Math.floor(Math.random() * 100000 + 1);
-  },
+	generate() {
+		return Math.floor(Math.random() * 100000 + 1);
+	},
 };
 
 export const mockCatsData = [
-  {
-    User: {
-      id: randomId.generate(),
-      userId: faker.person.firstName(),
-      content: faker.lorem.paragraph(),
-      myContent: false,
-    },
-    images: Array(Math.floor(Math.random() * 4) + 2)
-      .fill()
-      .map(() => faker.image.cats(650, 420, true)),
-    Comments: Array(Math.floor(Math.random() * 4) + 1)
-      .fill()
-      .map(() => {
-        return {
-          id: randomId.generate(),
-          nickname: faker.person.firstName(),
-          myComment: false,
-        };
-      }),
-  },
+	{
+		User: {
+			id: randomId.generate(),
+			userId: faker.person.firstName(),
+			content: faker.lorem.paragraph(),
+			myContent: false,
+		},
+		images: Array(Math.floor(Math.random() * 4) + 2)
+			.fill()
+			.map(() => faker.image.cats(650, 420, true)),
+		Comments: Array(Math.floor(Math.random() * 4) + 1)
+			.fill()
+			.map(() => {
+				return {
+					id: randomId.generate(),
+					nickname: faker.person.firstName(),
+					myComment: false,
+				};
+			}),
+	},
 ];
 
 // export const catsMock = (number) => {
